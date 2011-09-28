490ebaee907b78ec9872ebcff0b1c4070972d94d
yaxi1984 <yaxi1984@6c7611ac-2e8c-11df-a76b-1fcfea12351c>
2011-09-28 03:35:53 +0000

    *) Add watchdog driver;

--- avr-base/drv/drv_watchdog.h
+++ avr-base/drv/drv_watchdog.h
@@ -29,8 +29,90 @@
 #define __DRV_WATCHDOG_H
 
 
-#define DRV_WATCHDOG_Enable()   /* do nothing */
-#define DRV_WATCHDOG_Disable()  /* do nothing */
+/* ATmega16 Watchdog Prescale Definition:
+ *
+ *  Prescale    VCC(3.0v)     VCC(5.0v)
+ * ----------  -----------  ------------
+ *    0x0         17.1ms        16.3ms
+ *    0x1         34.3ms        32.5ms
+ *    0x2         68.5ms        65.0ms
+ *    0x3         0.14s         0.13s
+ *    0x4         0.27s         0.26s
+ *    0x5         0.55s         0.52s
+ *    0x6         1.1s          1.0s
+ *    0x7         2.2s          2.1s
+ */
+#define DRV_WATCHDOG_Prescale   0x7
+
+
+/******************************************************************************
+ * FUNCTION NAME:
+ *      DRV_WATCHDOG_Enable
+ * DESCRIPTION:
+ *      Enable Watchdog Timer.
+ * PARAMETERS:
+ *      N/A
+ * RETURN:
+ *      N/A
+ * NOTES:
+ *      N/A
+ * HISTORY:
+ *      2010.1.27        Panda.Xiong         Create/Update
+ *****************************************************************************/
+#define DRV_WATCHDOG_Enable()                                               \
+    do {                                                                    \
+        WDTCR = DRV_WATCHDOG_Prescale; /* set watchdog prescale */          \
+        WDTCR_WDE = 1;                 /* enable watchdog       */          \
+    } while (0)
+
+/******************************************************************************
+ * FUNCTION NAME:
+ *      DRV_WATCHDOG_Disable
+ * DESCRIPTION:
+ *      Disable Watchdog Timer.
+ * PARAMETERS:
+ *      N/A
+ * RETURN:
+ *      N/A
+ * NOTES:
+ *      N/A
+ * HISTORY:
+ *      2010.1.27        Panda.Xiong         Create/Update
+ *****************************************************************************/
+#define DRV_WATCHDOG_Disable()                                              \
+    do {                                                                    \
+        BOOL bIntStatus;                                                    \
+                                                                            \
+        /* lock global interrupt */                                         \
+        bIntStatus = DRV_INT_LockGlobalInterrupt();                         \
+                                                                            \
+        /* kick watchdog timer, to prevent Watchdog Timer timeout */        \
+        DRV_WATCHDOG_Kick();                                                \
+                                                                            \
+        /* set WDE bit, to make sure watchdog can be disabled */            \
+        WDTCR_WDTOE = 1;                                                    \
+                                                                            \
+        /* disable watchdog */                                              \
+        WDTCR_WDE = 0;                                                      \
+                                                                            \
+        /* unlock global interrupt */                                       \
+        DRV_INT_UnlockGlobalInterrupt(bIntStatus);                          \
+    } while (0)
+
+/******************************************************************************
+ * FUNCTION NAME:
+ *      DRV_WATCHDOG_Kick
+ * DESCRIPTION:
+ *      Kick Watchdog Timer, to prevent watchdog timeout.
+ * PARAMETERS:
+ *      N/A
+ * RETURN:
+ *      N/A
+ * NOTES:
+ *      N/A
+ * HISTORY:
+ *      2010.1.27        Panda.Xiong         Create/Update
+ *****************************************************************************/
 #define DRV_WATCHDOG_Kick()     __watchdog_reset()
 
 
