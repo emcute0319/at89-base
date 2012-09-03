77decfecbd2451d638f3bff9bd6ab36d728c6df9
yaxi1984 <yaxi1984@6c7611ac-2e8c-11df-a76b-1fcfea12351c>
2012-09-03 14:17:22 +0000

    *) Add release_build.bat

---
+++ avr-base/tools/converter/release_build.bat
@@ -0,0 +1,26 @@
+@echo off
+
+:: ===========================================================================
+::  These lines can be changed to fit product usage.
+:: ===========================================================================
+
+:: Define python install directory
+set PYTHON_DIR=%COMPILER_BASE%\python
+
+:: Define python entry file name at here
+set ENTRY_FILE=converter.py
+
+:: Define generated exe file type
+::   --> console : The generated exe file with Console.
+::   --> windows : The generated exe file without Console.
+set EXE_FILE_TYPE=console
+
+
+
+:: ===========================================================================
+::  These lines are forbidden to be changed, while porting to another product.
+:: ===========================================================================
+
+path %PYTHON_DIR%;%path%
+python %PYTHON_DIR%\py2exe_setup.py %ENTRY_FILE% --%EXE_FILE_TYPE%
+
